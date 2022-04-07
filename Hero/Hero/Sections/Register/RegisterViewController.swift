86b3986a6427bf241b22c25eb6740cf65e9630db
Usr_Prime <>
2022-04-07 11:13:24 -0700

    Criação e Configuração inicial da tela de Cadastro

---
+++ Hero/Hero/Sections/Register/RegisterViewController.swift
@@ -0,0 +1,25 @@
+//
+//  RegisterViewController.swift
+//  Hero
+//
+//  Created by Usr_Prime on 07/04/22.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(red: 0.15, green: 0.15, blue: 0.15, alpha: 0.60)
+        
+        setupView()
+        setupConstraints()
+    }
+    func setupView() {
+        
+    }
+    func setupConstraints() {
+        
+    }
+}
---
+++ Hero/Hero/Sections/Register/RegisterViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  RegisterViewModel.swift
+//  Hero
+//
+//  Created by Usr_Prime on 07/04/22.
+//
+
+import Foundation
