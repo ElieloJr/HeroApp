2801fc3045827d3905c76f33ca75cd497ed79a8b
Usr_Prime <>
2022-04-11 11:58:35 -0700

    4.3 Criar HomeViewController

---
+++ Hero/Hero/Sections/Home/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  Hero
+//
+//  Created by Usr_Prime on 11/04/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    let lightgrey = UIColor(red: 0.15, green: 0.15, blue: 0.15, alpha: 1.00)
+    let darkGrey = UIColor(red: 0.1, green: 0.1, blue: 0.1, alpha: 1.00)
+    let moreLightgrey = UIColor(red: 0.60, green: 0.60, blue: 0.60, alpha: 1.00)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = darkGrey
+    }
+}
