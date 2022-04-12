3a600dcb105759d9f2299c09f7e42aa6b0d9466e
Usr_Prime <>
2022-04-12 11:05:58 -0700

    6.7 Criação da TableViewCell

---
+++ Hero/Hero/View/FavoriteTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FavoriteTableViewCell.swift
+//  Hero
+//
+//  Created by Usr_Prime on 12/04/22.
+//
+
+import UIKit
+
+class FavoriteTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
