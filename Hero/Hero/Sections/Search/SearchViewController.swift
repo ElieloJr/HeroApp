f435580ff46f4d10a169b3b84201bd1a83ad51a1
Usr_Prime <>
2022-04-19 12:24:48 -0700

    7.2 Criação do Back Button para a home

--- Hero/Hero/Sections/Search/SearchViewController.swift
+++ Hero/Hero/Sections/Search/SearchViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    let lightgrey = UIColor(red: 0.15, green: 0.15, blue: 0.15, alpha: 1.00)
     let darkGrey = UIColor(red: 0.1, green: 0.1, blue: 0.1, alpha: 1.00)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = darkGrey
+        
+        setupNavigationBar()
     }
-    override var preferredStatusBarStyle: UIStatusBarStyle { return .lightContent }
+    func setupNavigationBar() {
+        let leftButton: UIButton = UIButton(frame: CGRect(x: 0, y: 0, width: 30, height: 20))
+        leftButton.setImage(UIImage(systemName: "arrow.left"), for: .normal)
+        leftButton.tintColor = .white
+        leftButton.contentVerticalAlignment = .fill
+        leftButton.contentHorizontalAlignment = .fill
+        leftButton.addTarget(self, action: #selector(backButton), for: .touchUpInside)
+        
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: leftButton)
+    }
+    @objc func backButton() {
+        dismiss(animated: true, completion: nil)
+    }
 }
