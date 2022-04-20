4bd17431ce01d2b1bab388bdf2c6a84124bbcdd7
Usr_Prime <>
2022-04-20 06:26:17 -0700

    8.2 Implementação do Favorite Button

--- Hero/Hero/Sections/Detail/DetailViewController.swift
+++ Hero/Hero/Sections/Detail/DetailViewController.swift
@@ -9,10 +9,44 @@ import UIKit
 
 class DetailViewController: UIViewController {
     
+    let lightgrey = UIColor(red: 0.15, green: 0.15, blue: 0.15, alpha: 1.00)
     let darkGrey = UIColor(red: 0.1, green: 0.1, blue: 0.1, alpha: 1.00)
+    
+    lazy var favoriteButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "heart"), for: .normal)
+        button.tintColor = .white
+        button.setTitle("   Add to Favorites", for: .normal)
+        button.titleLabel?.font = button.titleLabel?.font.withSize(20)
+        button.backgroundColor = lightgrey
+        button.layer.cornerRadius = 10
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(addToFavoritesButton), for: .touchUpInside)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = darkGrey
+        
+        setupView()
+        setupConstraints()
+    }
+    private func setupView() {
+        view.addSubview(favoriteButton)
+    }
+    private func setupConstraints() {
+        let favoriteButtonConstraints = [
+            favoriteButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -(self.view.frame.width/10)),
+            favoriteButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: self.view.frame.width/5),
+            favoriteButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -(self.view.frame.width/5)),
+            favoriteButton.heightAnchor.constraint(equalToConstant:  self.view.frame.width/7.5)
+        ]
+        
+        NSLayoutConstraint.activate(favoriteButtonConstraints)
+    }
+    
+    @objc func addToFavoritesButton() {
+        favoriteButton.setImage(UIImage(systemName: "heart.fill"), for: .normal)
     }
 }
