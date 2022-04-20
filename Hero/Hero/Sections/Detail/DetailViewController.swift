06562b45ef7568bb2ac92de3707d69fda9393039
Usr_Prime <>
2022-04-20 06:56:17 -0700

    Implementação do ScrollVIew

--- Hero/Hero/Sections/Detail/DetailViewController.swift
+++ Hero/Hero/Sections/Detail/DetailViewController.swift
@@ -12,6 +12,11 @@ class DetailViewController: UIViewController {
     let lightgrey = UIColor(red: 0.15, green: 0.15, blue: 0.15, alpha: 1.00)
     let darkGrey = UIColor(red: 0.1, green: 0.1, blue: 0.1, alpha: 1.00)
     
+    lazy var detailsScrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
     lazy var favoriteButton: UIButton = {
         let button = UIButton()
         button.setImage(UIImage(systemName: "heart"), for: .normal)
@@ -33,9 +38,16 @@ class DetailViewController: UIViewController {
         setupConstraints()
     }
     private func setupView() {
+        view.addSubview(detailsScrollView)
         view.addSubview(favoriteButton)
     }
     private func setupConstraints() {
+        let detailsScrollViewConstraints = [
+            detailsScrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            detailsScrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            detailsScrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            detailsScrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ]
         let favoriteButtonConstraints = [
             favoriteButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -(self.view.frame.width/10)),
             favoriteButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: self.view.frame.width/5),
@@ -43,9 +55,9 @@ class DetailViewController: UIViewController {
             favoriteButton.heightAnchor.constraint(equalToConstant:  self.view.frame.width/7.5)
         ]
         
+        NSLayoutConstraint.activate(detailsScrollViewConstraints)
         NSLayoutConstraint.activate(favoriteButtonConstraints)
     }
-    
     @objc func addToFavoritesButton() {
         favoriteButton.setImage(UIImage(systemName: "heart.fill"), for: .normal)
     }
