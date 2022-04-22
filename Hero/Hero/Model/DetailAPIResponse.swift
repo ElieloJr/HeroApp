edc3fc847af855d2523f8312912d457eb89147d7
Usr_Prime <>
2022-04-22 09:18:09 -0700

    9.2 Correção no struct e implementação de sistema para requisições

--- Hero/Hero/Model/DetailAPIResponse.swift
+++ Hero/Hero/Model/DetailAPIResponse.swift
@@ -6,46 +6,76 @@
 //
 
 import Foundation
-import UIKit
 
-class DetailAPIResponse {
-    struct DetailAPIResponse: Codable {
-        let results: [Details]
+struct DetailAPIResponse: Codable {
+    let results: [Details]
+}
+struct Details: Codable {
+    let id: String
+    let name: String
+    let powerstats: Powerstats
+    let biography: Biography
+    let appearance: Appearance
+    let work: Work
+    let connections: Connections
+    let image: Image
+}
+struct Powerstats: Codable {
+    let intelligence: String
+    let strength: String
+    let speed: String
+    let durability: String
+    let power: String
+    let combat: String
+}
+struct Biography: Codable {
+    let fullName: String
+    let placeOfAppearance: String
+    let firstAppearance: String
+    let publisher: String
+    
+    enum CodingKeys: String, CodingKey {
+        case fullName = "full-name"
+        case placeOfAppearance = "place-of-birth"
+        case firstAppearance = "first-appearance"
+        case publisher
     }
-    struct Details: Codable {
-        let id: Int
-        let name: String
-        let powerstats: Powerstats
-        let biography: Biography
-        let appearance: Appearance
-        let work: Work
-        let connections: Connections
-        let image: Image
+    static func codingKey<Value>(for KeyPath: KeyPath<Biography, Value>) -> String? {
+        let codingKey: CodingKeys
+        switch KeyPath {
+        case \Biography.fullName:
+            codingKey = .fullName
+        case \Biography.placeOfAppearance:
+            codingKey = .placeOfAppearance
+        case \Biography.firstAppearance:
+            codingKey = .firstAppearance
+        default: return nil
+        }
+        return codingKey.rawValue
     }
-    struct Powerstats: Codable {
-        let inteligence: String
-        let strength: String
-        let speed: String
-        let durability: String
-        let power: String
-        let combat: String
+}
+struct Appearance: Codable {
+    let race: String
+}
+struct Work: Codable {
+    let base: String
+}
+struct Connections: Codable {
+    let groupAffiliation: String
+    
+    enum CodingKeys: String, CodingKey {
+        case groupAffiliation = "group-affiliation"
     }
-    struct Biography: Codable {
-//        let full-name: String
-//        let place-of-appearance: String
-//        let first-appearance: String
-        let publisher: String
+    static func codingKey<Value>(for KeyPath: KeyPath<Connections, Value>) -> String? {
+        let codingKey: CodingKeys
+        switch KeyPath {
+        case \Connections.groupAffiliation:
+            codingKey = .groupAffiliation
+        default: return nil
+        }
+        return codingKey.rawValue
     }
-    struct Appearance: Codable {
-        let race: String
-    }
-    struct Work: Codable {
-        let base: String
-    }
-    struct Connections: Codable {
-//        let group-affiliation: String
-    }
-    struct Image: Codable {
-        let url: String
-    }
+}
+struct Image: Codable {
+    let url: String
 }
